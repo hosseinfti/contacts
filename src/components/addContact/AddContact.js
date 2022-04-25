a1d0715c06db3c06f3c52842cdda77bd119a6c01
Hossein <hosseinfti@yahoo.com>
2022-04-25 15:41:33 +0430

    add delete button

--- src/components/addContact/AddContact.js
+++ src/components/addContact/AddContact.js
@@ -8,52 +8,124 @@ class AddContact extends Component {
       name: "",
       family: "",
       numbers: "",
+      contacts: this.props.contacts,
     };
   }
-  addContactHandler = () => {
 
+  addContactHandler = () => {
+    let uniqID = new Date().valueOf();
+    let newContact = {
+      id: uniqID,
+      name: this.state.name,
+      family: this.state.family,
+      numbers: this.state.numbers,
+    };
+    if (!this.state.name || !this.state.family || !this.state.numbers) {
+      alert("تمامی فیلد‌ها پر شود");
+    } else {
+      let newList = this.state.contacts;
+      newList.push(newContact);
+
+      this.setState(
+        {
+          contacts: newList,
+        },
+        () => {
+          this.setState({
+            name: "",
+            family: "",
+            numbers: "",
+          });
+        }
+      );
+    }
+  };
+
+  inputsChangedHandler = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  contactDeleteHandler = (itemToBeRemove) => {
+    let newList = this.state.contacts.filter((i) => {
+      return i.id !== itemToBeRemove;
+    });
+    this.setState({
+      contacts: newList,
+    });
+  };
+
+  contactEditHandler = () => {};
+
+  componentDidMount() {
+    let lastContact = localStorage.getItem("contact");
+    this.setState({
+      contacts: ("contact", JSON.parse(lastContact)),
+    });
   }
-  nameChangeHandler = (e) => {
-    this.setState({
-      name: e.target.value,
-    });
-  };
-  familyChangeHandler = (e) => {
-    this.setState({
-      family: e.target.value,
-    });
-  };
-  numberChangeHandler = (e) => {
-    this.setState({
-      numbers: e.target.value,
-    });
-  };
+  componentDidUpdate() {
+    localStorage.setItem("contact", JSON.stringify(this.state.contacts));
+  }
+
   render() {
     return (
-      <tr>
-        <td>
-          <input
-            type="text"
-            value={this.state.contactInput}
-            onChange={this.nameChangeHandler}
-          />
-        </td>
-        <td>
-          <input
-            type="text"
-            value={this.state.contactInput}
-            onChange={this.familyChangeHandler}
-          />
-        </td>
-        <td>
-          <input
-            type="text"
-            value={this.state.contactInput}
-            onChange={this.numberChangeHandler}
-          />
-        </td>
-        <button className="addButton" onSubmit={this.addContactHandler} > اضافه </button>
-      </tr>
+      <>
+        <tr>
+          <td>
+            <input
+              type="text"
+              value={this.state.name}
+              id="name"
+              onChange={this.inputsChangedHandler}
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              value={this.state.family}
+              id="family"
+              onChange={this.inputsChangedHandler}
+            />
+          </td>
+          <td>
+            <input
+              type="number"
+              value={this.state.numbers}
+              id="numbers"
+              onChange={this.inputsChangedHandler}
+            />
+          </td>
+          <td>
+            <button className="addButton" onClick={this.addContactHandler}>
+              اضافه
+            </button>
+          </td>
+        </tr>
+        <tr>
+        <td><input type="text" /></td>
+        <td><input type="text" /></td>
+        <td><input type="number" /></td>
+        <td></td>
+        </tr>
+        {this.state.contacts.map((i) => {
+          return (
+            <tr id={i.id} key={i.numbers}>
+              <td>{i.name}</td>
+              <td>{i.family}</td>
+              <td>{i.numbers}</td>
+              <td>
+                <button onClick={() => this.contactDeleteHandler(i.id)}>
+                  X
+                </button>
+                <button onClick={() => this.contactEditHandler(i.id)}>
+                  Edit
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </>
     );
   }
 }
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,49 +1,106 @@
 import React, { Component } from "react";
 import AddContact from "../addContact/AddContact";
-import "./table.css";
+import "./table.scss";
 import data from "../../json/data.json";
-import axios from "axios"
+import axios from "axios";
 
 class Table extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      contacts: data
+      name: "",
+      family: "",
+      numbers: "",
+      contacts: data,
     };
-  
   }
 
+  addContactHandler = () => {
+    let uniqID = new Date().valueOf();
+    let newContact = {
+      id: uniqID,
+      name: this.state.name,
+      family: this.state.family,
+      numbers: this.state.numbers,
+    };
+    if (!this.state.name || !this.state.family || !this.state.numbers) {
+      alert("تمامی فیلد‌ها پر شود");
+    } else {
+      let newList = this.state.contacts;
+      newList.push(newContact);
+
+      this.setState(
+        {
+          contacts: newList,
+        },
+        () => {
+          this.setState({
+            name: "",
+            family: "",
+            numbers: "",
+          });
+        }
+      );
+    }
+  };
+
+  inputsChangedHandler = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  contactDeleteHandler = (itemToBeRemove) => {
+    let newList = this.state.contacts.filter((i) => {
+      return i.id !== itemToBeRemove;
+    });
+    this.setState({
+      contacts: newList,
+    });
+  };
+
+  contactEditHandler = () => {};
+
   componentDidMount() {
-    axios.get(data)
-    .then((res) => {
+    axios.get("../../json/data.json").then((res) => {
       console.log(res);
-    })
+    });
+    let lastContact = localStorage.getItem("contact");
+    this.setState({
+      contacts: ("contact", JSON.parse(lastContact)),
+    });
+  }
+  componentDidUpdate() {
+    localStorage.setItem("contact", JSON.stringify(this.state.contacts));
   }
 
   render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>نام</th>
-            <th>نام خانوادگی</th>
-            <th>شماره تلفن</th>
-          </tr>
-        </thead>
-        <tbody>
-          <AddContact />
-          {this.state.contacts.map((i) => {
-            return (
-              <tr key={i.numbers}>
-                <td>{i.name}</td>
-                <td>{i.family}</td>
-                <td>{i.numbers}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="Container">
+        <table>
+          <thead>
+            <tr>
+              <th>نام</th>
+              <th>نام خانوادگی</th>
+              <th>شماره تلفن</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <AddContact
+              contactDeleteHandler={this.contactDeleteHandler}
+              inputsChangedHandler={this.inputsChangedHandler}
+              addContactHandler={this.addContactHandler}
+              contacts={this.state.contacts}
+              name={this.state.name}
+              family={this.state.family}
+              id={this.state.id}
+              numbers={this.state.numbers}
+            />
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
