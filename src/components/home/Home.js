bb4f198cb4e535fc6c2589359ac5a18440f8bd3d
Hossein <hosseinfti@yahoo.com>
2022-05-16 15:11:29 +0430

    create black logo of lifeweb

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import AddContactInput from "../addContactInput/AddContactInput";
+import Logo from "../logo/Logo";
 import "./home.scss";
 
 class Home extends Component {
@@ -10,6 +11,7 @@ class Home extends Component {
     return (
       <>
         <div className="wellcome">
+          <Logo />
           <h1>Wellcome</h1>
           <AddContactInput />
           <Link to="/contacts">
---
+++ src/components/logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./logo.scss"
+
+const Logo = () => {
+    return (
+        <div className='logo'>
+            <div className='sides side1'></div>
+            {/* <div className='sides side2'></div>
+            <div className='sides side3'></div>
+            <div className='sides side4'></div>
+            <div className='sides side5'></div>
+            <div className='sides side6'></div> */}
+            <div className='insideShape'></div>
+        </div>
+    );
+};
+
+export default Logo;
