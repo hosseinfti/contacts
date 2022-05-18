8827f378c2285ce9376bcee13c743fa4f8f78965
Hossein <hosseinfti@yahoo.com>
2022-05-18 18:23:03 +0430

    add new route for user info

--- src/components/itemContact/ItemContact.js
+++ src/components/itemContact/ItemContact.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./itemContact.scss";
+import { Link } from "react-router-dom";
 import noProfile from "../../assets/image/user.svg";
 
 class ItemContact extends Component {
@@ -22,51 +23,53 @@ class ItemContact extends Component {
       <>
         {searchedContact.map((i, index) => {
           return (
-            <div key={i.id} className="contactItem">
-              <div>{String(index + 1).padStart(2, "0")}).</div>
-              <div className="profilePicture">
-                <img src={noProfile} alt="profilePicture"/>
-              </div>
-              <div className="nameFamily">
-                <div
-                  id="editName"
-                  onClick={(e) => inlineEditHandler(e, i.name)}
-                >
-                  {i.name}
+            <Link to="/user" >
+              <div key={i.id} className="contactItem">
+                <div>{String(index + 1).padStart(2, "0")}).</div>
+                <div className="profilePicture">
+                  <img src={noProfile} alt="profilePicture" />
                 </div>
-                <div
-                  id="editFamily"
-                  onClick={(e) => inlineEditHandler(e, i.family)}
-                >
-                  {i.family}
+                <div className="nameFamily">
+                  <div
+                    id="editName"
+                    onClick={(e) => inlineEditHandler(e, i.name)}
+                  >
+                    {i.name}
+                  </div>
+                  <div
+                    id="editFamily"
+                    onClick={(e) => inlineEditHandler(e, i.family)}
+                  >
+                    {i.family}
+                  </div>
+                </div>
+                <div>
+                  {String(i.numbers[0]).substring(7, 11) +
+                    "***" +
+                    String(i.numbers[0]).substring(0, 4)}
+                </div>
+                <div className="itemBTNS">
+                  <button
+                    className="deleteBTN"
+                    onClick={() => contactDeleteHandler(i.id)}
+                  >
+                    X
+                  </button>
+                  <button
+                    className="editBTN"
+                    onClick={() => contactEditHandler(i)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="infoBTN"
+                    onClick={() => contactInfoHandler(i)}
+                  >
+                    i
+                  </button>
                 </div>
               </div>
-              <div>
-                {String(i.numbers[0]).substring(7, 11) +
-                  "***" +
-                  String(i.numbers[0]).substring(0, 4)}
-              </div>
-              <div className="itemBTNS">
-                <button
-                  className="deleteBTN"
-                  onClick={() => contactDeleteHandler(i.id)}
-                >
-                  X
-                </button>
-                <button
-                  className="editBTN"
-                  onClick={() => contactEditHandler(i)}
-                >
-                  Edit
-                </button>
-                <button
-                  className="infoBTN"
-                  onClick={() => contactInfoHandler(i)}
-                >
-                  i
-                </button>
-              </div>
-            </div>
+            </Link>
           );
         })}
       </>
--- src/components/routing/Routing.js
+++ src/components/routing/Routing.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../home/Home";
 import Table from "../table/Table";
+import User from "../user/User";
 
 
 class Routing extends Component {
@@ -11,6 +12,7 @@ class Routing extends Component {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/contacts" element={<Table />} />
+            <Route exact path="/user" element={<User />} />
           </Routes>
         </Router>
     );
