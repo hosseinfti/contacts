30a544fd7bc5de2df25ab0a5c02d4cb94e151b8e
Hossein <79690564+hosseinfti@users.noreply.github.com>
2023-03-30 23:32:49 +0330

    clean code

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import AddContactInput from "../addContactInput/AddContactInput";
 import Logo from "../logo/Logo";
-// import NewLogo from "../logo/NewLogo";
 import "./home.scss";
 
 class Home extends Component {
